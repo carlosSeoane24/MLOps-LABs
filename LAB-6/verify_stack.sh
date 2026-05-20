98ade8ae8a65147de5b0636c91323f6d691ecd9b
carlosSeoane24 <carlos.seo.fer@gmail.com>
2026-05-20 20:05:38 +0200

    Delete LAB-6 directory

--- LAB-6/verify_stack.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "📦 Contenedores activos:"
-docker ps --format "table {{.Names}}\t{{.Status}}\t{{.Ports}}"
-echo ""
-echo "📡 Ping al Backend (FastAPI)..."
-curl -s http://localhost:8000/health
-echo ""
